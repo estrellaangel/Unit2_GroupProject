5075ad7e2e299312b5d8bd18e00b844a0ce7fa91
Jadyn Calhoun <jaycal2013@gmail.com>
2020-09-11 15:23:03 -0700

    fix the repo

---
+++ index.js
@@ -0,0 +1,14 @@
+
+var bits = prompt("How many bits are there?");
+var bytes = bits / 8;
+var kilobytes = bytes / 1024;
+var megabytes = kilobytes / 1024;
+var gigabytes = megabytes / 1024;
+var terabytes = gigabytes / 1024;
+console.log(`bytes: ${bytes}`);
+console.log(`kilobytes: ${kilobytes}`);
+console.log(`megabytes: ${megabytes}`);
+console.log(`gigabytes: ${gigabytes}`);
+console.log(`terabytes: ${terabytes}`);
+
+
