4ebb722031a1bbaf436bbdd73e43900df9a67364
Estrella Angel <ella34138@gmail.com>
2020-09-14 13:55:41 -0700

    Test

--- Unit2_GroupProject/Converter.js
+++ Unit2_GroupProject/Converter.js
@@ -1,20 +1,17 @@
-let number = prompt("What is the decimal number");
+let number = prompt("What is the decimal number?");
 let finalString;
 
-<<<<<<< Updated upstream
-=======
 let binary =(number);
 
->>>>>>> Stashed changes
 
 binary = number
-
 
 while(binary>0){
     bfinalString.concat(`${binary%2}`);
     binary = binary - (binary%2);
     binary = binary/2;
 }
+
 console.log(finalString);
 
 
