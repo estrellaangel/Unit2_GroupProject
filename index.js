ac370074d719e3241a3be88cd02bdc4d6105858d
Estrella Angel <ella34138@gmail.com>
2020-09-14 13:24:58 -0700

    Javascript

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+let number = prompt("What is the decimal number");
+let finalString;
+
+<<<<<<< Updated upstream
+=======
+let binary =(number);
+
+>>>>>>> Stashed changes
+
+binary = number
 
 
+while(binary>0){
+    bfinalString.concat(`${binary%2}`);
+    binary = binary - (binary%2);
+    binary = binary/2;
+}
+console.log(finalString);
+
+
+Math.FLoor
