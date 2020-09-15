56d5c066b18c196e47b51c58104cce78315a15d1
Estrella Angel <ella34138@gmail.com>
2020-09-15 01:01:46 -0700

    Final Calcs

--- Unit2_GroupProject/Converter.js
+++ Unit2_GroupProject/Converter.js
@@ -1,18 +1,38 @@
 let number = prompt("What is the decimal number?");
-let finalString;
 
-let binary =(number);
+let binary = Number(number);
 
+let binaryArray =[];
 
-binary = number
-
-while(binary>0){
-    bfinalString.concat(`${binary%2}`);
+while (binary>0){
+    binaryArray.push(binary%2);
     binary = binary - (binary%2);
     binary = binary/2;
 }
 
-console.log(finalString);
+binaryArray.reverse();
+var binaryStringC = binaryArray.toString();
+var binaryString = binaryStringC.replace(/,/g,``);
+console.log(binaryString);
 
 
-Math.FLoor
+let hexadecimal = Number(number);
+
+let hexadecimalArray =[];
+
+let hexadecimalRemainder;
+
+while (hexadecimal>0){
+    hexadecimalArray.push(hexadecimal%16);
+    hexadecimal = hexadecimal - (hexadecimal%16);
+    hexadecimal = hexadecimal/16;
+}
+
+hexadecimalArray.reverse();
+
+var hexadecimalStringC = hexadecimalArray.toString();
+
+var finalHexadecimalArray = hexadecimalStringC.replace(`10`,`A`).replace(`11`,`B`).replace(`12`,`C`).replace(`13`,`D`).replace(`14`,`E`).replace(`15`,`F`);
+
+var hexadecimalString = finalHexadecimalArray.replace(/,/g,``);
+console.log(hexadecimalString);
